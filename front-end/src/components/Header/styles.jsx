fa12800bdbf390628aff7fe1f13ca1e6c47fb0f0
eazsilver <vdomobv@naver.com>
2023-07-27 17:12:34 +0900

    <feat>[#257-LJE] s헤더 컴포넌트 생성 및 각 페이지에 추가

--- front-end/src/components/Header/styles.jsx
+++ front-end/src/components/Header/styles.jsx
@@ -2,6 +2,29 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   display: flex;
+  justify-content: space-between;
+  box-shadow: 0 1px 10px -5px black;
+
+  .logo{
+    width:150px;
+  }
+
+  .links{
+    display: flex;
+    margin-right: 20px;
+  }
+
+  .active{
+    font-weight: bold;
+  }
+
+  a{
+    margin : auto 20px;
+    text-decoration: none;
+    font-size: 20px;
+    color: #000000;
+    }
+  }
 `
 
 export default Wrapper;
