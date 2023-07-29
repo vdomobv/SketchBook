49160fcbc1b5bd30105ba1a3a4a53740ddea1519
김진주 <zizu4256@naver.com>
2023-07-29 16:52:09 +0900

    <feat>[#229-KJJ] 그리드 형식 및 더미 데이터 생성

--- front-end/src/pages/Books/index.jsx
+++ front-end/src/pages/Books/index.jsx
@@ -1,9 +1,43 @@
+// import { useState} from 'react';
+
+// styled_components
+// import Wrapper from './styles';
+
+// bootstrap
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+// Components
 import Header from "../../components/Header";
 
 function Books() {
   return <div>
     <Header />
     <h1>Books</h1>
+    <p>검색기능 넣을겁니당</p>
+  <br />
+
+{/* 카드 형식 */}
+<div class="container">
+    <Row xs={1} md={2} lg={3} className="g-4">
+      {Array.from({ length: 6 }).map((_, idx) => (
+        <Col key={idx}>
+          <Card style={{ width: '25rem' }}>
+            {/* 책 표지 */}
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>제목 입니당</Card.Title>  
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+</div>
+    {/* <Wrapper>
+
+    </Wrapper> */}
+
   </div>;
 }
 
