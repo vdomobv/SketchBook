d802c5de5d7fc9e4e188c666f474fd08798e5333
김진주 <zizu4256@naver.com>
2023-07-29 16:52:09 +0900

    <feat>[#229-KJJ] 그리드 형식 및 더미 데이터 생성

---
+++ front-end/src/pages/Profile/styles.jsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+margin-top: 100px;
+display : flex;
+justify-content : space-evenly;
+
+.grid{
+    width : 40%;
+}
+
+img{
+    width: 100%;
+    height: 50%;
+}
+
+.box {
+    background-color: #F8F8F8;
+    border-radius: 10px;
+    padding: 50px;
+  }
+
+  input{
+    padding: 15px;
+    background-color: #E6E6E6;
+    color: black;
+  }
+
+  .btn{
+    background-color: #FFEE59;
+    padding: 10px 0;
+    font-weight : bold ;
+  }
+
+  h2{
+    font-weight : bold;
+  }
+
+  h2 span{
+    color: #A451F7;
+  }
+
+  .message{
+    font-size : 12px;
+  }
+
+
+`
+
+export default Wrapper;
