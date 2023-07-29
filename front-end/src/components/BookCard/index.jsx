47025ac902d55b74e60da63288ebba70d1ff3b46
김진주 <zizu4256@naver.com>
2023-07-30 00:32:00 +0900

    <bug>[#229-KJJ] 버튼 호버 수정

--- front-end/src/components/BookCard/index.jsx
+++ front-end/src/components/BookCard/index.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-
 import Card from 'react-bootstrap/Card';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import { Wrapper } from './styles';
 
 function BookCard({ book }) {
   const [showModal, setShowModal] = useState(false);
@@ -29,52 +29,34 @@ function BookCard({ book }) {
           {/* <Modal.Title>{book.title}</Modal.Title> */}
         </Modal.Header>
         <Modal.Body>
-          <div style={{ display: 'flex' }}>
+          <Wrapper>
             <div style={{ flex: 1 }}>
               <img src={book.bookcover} alt={book.title} style={{ width: '30rem', height: '35rem' }} />
             </div>
             <div style={{ flex: 2, padding: '10px 40px 10px 40px' }}>
-              <h3>{book.title}</h3>
-              <h5 style={{ textAlign: 'right' }}>{book.writer}</h5>
+              <h2>{book.title}</h2>
+              <h5>{book.writer} 작가</h5>
               <br />
               <p>{book.summary}</p>
 
               <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '100px' }}>
                 <Button
                   variant="outline-primary"
-                  style={{
-                    width: '200px',
-                    borderColor: '#9D4FE0',
-                    marginBottom: '10px',
-                    color: '#9D4FE0',
-                    ':hover': {
-                      backgroundColor: '#9D4FE0',
-                      color: '#ffffff',
-                    },
-                  }}
+                  className="custom-button-style"
                   onClick={() => console.log('첫 번째 버튼 클릭')}
                 >
                   캐릭터 출력하기
                 </Button>
                 <Button
                   variant="outline-primary"
-                  style={{
-                    width: '200px',
-                    borderColor: '#9D4FE0',
-                    color: '#9D4FE0',
-                    ':hover': {
-                      backgroundColor: '#9D4FE0',
-                      color: '#ffffff',
-                    },
-                  }}
+                  className="custom-button-style"
                   onClick={() => console.log('두 번째 버튼 클릭')}
                 >
                   시작하기
                 </Button>
               </div>
-
            </div>
-          </div>
+          </Wrapper>
         </Modal.Body>
       </Modal>
     </>
