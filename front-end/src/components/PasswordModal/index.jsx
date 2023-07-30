094e0d513d081249c583a16ab4d37c09f490e6e1
eazsilver <vdomobv@naver.com>
2023-07-30 15:12:34 +0900

    <feat>[#256-LJE] 비밀번호 재설정 팝업 UI 구현

--- front-end/src/components/PasswordModal/index.jsx
+++ front-end/src/components/PasswordModal/index.jsx
@@ -1,12 +1,37 @@
-import Modal from "react-modal";
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import Wrapper from './styles';
 
 function PasswordModal(props) {
+
   return (
-    <div>
-      <Modal isOpen={props.isModalOpen}>
-        This is Modal content
-      </Modal>
-    </div>
+
+    <Modal show={props.isModalOpen} onHide={props.closeModal} centered>
+      <Wrapper
+      >
+        <Modal.Header closeButton>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>비밀번호 찾기</h4>
+          <p>
+            계정에 등록된 이메일을 입력하시면 <br />
+            이메일로 임시 비밀번호를 보내드려요.
+          </p>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Control
+                type="email"
+                placeholder="이메일 주소"
+                autoFocus
+              />
+            </Form.Group>
+          </Form>
+          <button className='sendBtn' type="button" onClick={props.closeModal}>
+            이메일로 임시비밀번호 받기
+          </button>
+        </Modal.Body>
+      </Wrapper>
+    </Modal>
   );
 }
 
--- front-end/src/components/PasswordModal/styles.jsx
+++ front-end/src/components/PasswordModal/styles.jsx
@@ -1,7 +1,37 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  
+text-align: center;
+
+.modal-content{
+    width : 350px;
+}
+
+.modal-header{
+    padding-bottom: 0;
+}
+
+h4{
+    font-weight: bold;
+    margin-bottom: 20px;
+}
+
+.modal-header{
+    border-bottom: none;
+}
+
+.modal-body{
+    padding-top: 0;
+}
+
+.sendBtn{
+    border-radius: 5px;
+    width : 100%;
+    border : none;
+    background-color: #FFEE59;
+    padding: 10px;
+    font-weight : bold ;
+}
 `
 
 export default Wrapper;
