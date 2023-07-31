e55719ff09d557e6b5f3bc77f251ea1b5aaf2790
jochanik <whcksdlr5909@naver.com>
2023-07-31 14:23:27 +0900

    <feat>[#286-JCI] 로그인 인증 Auth 기능 구현

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -86,5 +86,19 @@ userSchema.methods.generateToken = function (cb) {
   });
 };
 
+// auth 인증 - 복호화 (토큰을 디코드)
+userSchema.statics.findByToken = function(token, cb) {
+  var user = this
+
+  jwt.verify(token, 'secretToken', function(err, decoded) {
+    // 유저 아이디를 이용해서 유저를 찾은 다음에
+    // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+    user.findOne({"_id": decoded, "token": token}, function(err, user) {
+      if (err) return cb(err)
+      cb(null, user)
+    })
+  })
+}
+
 const User = mongoose.model("User", userSchema);
 module.exports = { User };
