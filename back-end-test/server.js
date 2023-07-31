d14fae41e5fea8702e6b3be53a58789d361c0e51
서정빈 <jeongb9716@gmail.com>
2023-07-31 17:35:00 +0900

    <first>[#287-SJB] Back-end간 API 수정 및 server 통일
    - S09P12C102/back-end-test
    - back-end 문법 통일 진행 중

---
+++ back-end-test/server.js
@@ -0,0 +1,45 @@
+// 회원정보, 동화정보는 MongoDB를 사용
+// OTP, 관절 포인트는 Redis를 사용
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const redis = require("redis");
+
+const app = express();
+dotenv.config();
+
+/* middleware */
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(helmet());
+
+/* PORT 지정 */
+const PORT = process.env.PORT;
+
+const { dbConnect } = require("./module/dbConnect");
+
+app.get("/", (req, res) => res.send("안녕하세요!"));
+
+const client = redis.createClient({
+  url: process.env.REDIS_URL,
+});
+
+// on the connection
+client.on("connect", () => console.log("Redis에 연결되었습니다."));
+
+client.connect();
+
+dbConnect();
+
+app.listen(PORT, () => console.log(`${PORT}번에 잘 접속했습니다.`));
