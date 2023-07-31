fb2da23c21b44aa1363c5d23b743265e9736be36
vdomobv <vdomobv@naver.com>
2023-07-31 17:01:09 +0900

    <feat>[#294-LJE] 캐릭터 프린트 기능 구현 - 캐릭터 틀 이미지 생성 - 책 상세 페이지에서 버튼 클릭시 프린트 창 팝업 - 프린트 자동 실행 - 프린트 페이지 팝업으로 변경으로 인해 App,js의 router 변경 - pages/Print 삭제

--- front-end/src/components/BookCard/index.jsx
+++ front-end/src/components/BookCard/index.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import Card from 'react-bootstrap/Card';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
-import { Wrapper, FontWrap } from './styles';
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import { Wrapper, FontWrap } from "./styles";
 
 function BookCard({ book }) {
   const [showModal, setShowModal] = useState(false);
@@ -15,54 +15,99 @@ function BookCard({ book }) {
     setShowModal(false);
   };
 
+  // 출력 버튼 클릭
+  const handlePrint = () => {
+    // 새창 생성
+    const printWindow = window.open("", "Print Window", "width=800,height=600");
+
+    // 창 페이지 구성
+    const printContent = `
+            <style>
+              img { width: 100%; height: auto; page-break-after: always;}
+            </style>
+            <img src="${process.env.PUBLIC_URL}/assets/character.jpg" alt="Character">
+      `;
+
+    printWindow.document.write(printContent);
+
+    // Close the document after printing is completed
+    printWindow.document.close();
+
+    // Trigger the print dialog
+    printWindow.print();
+  };
+
   return (
     <>
-    <FontWrap>
-        <Card style={{ width: '25rem', margin: '20px 0px 20px 0px'}} onClick={handleModalShow}>
-          <Card.Img variant="top" src={book.bookcover} style={{ width: '24.9rem', height: '30rem' }} />
+      <FontWrap>
+        <Card
+          style={{ width: "25rem", margin: "20px 0px 20px 0px" }}
+          onClick={handleModalShow}
+        >
+          <Card.Img
+            variant="top"
+            src={book.bookcover}
+            style={{ width: "24.9rem", height: "30rem" }}
+          />
           <Card.Body>
-          <Card.Title style={{ textAlign: 'center' }}>{book.title}</Card.Title>
-        </Card.Body>
+            <Card.Title style={{ textAlign: "center" }}>
+              {book.title}
+            </Card.Title>
+          </Card.Body>
         </Card>
-        </FontWrap>
+      </FontWrap>
 
-        <Modal show={showModal} onHide={handleModalClose} size="xl">
-          <Modal.Header closeButton>
-            {/* <Modal.Title>{book.title}</Modal.Title> */}
-          </Modal.Header>
-          <Modal.Body>
+      <Modal show={showModal} onHide={handleModalClose} size="xl">
+        <Modal.Header closeButton>
+          {/* <Modal.Title>{book.title}</Modal.Title> */}
+        </Modal.Header>
+        <Modal.Body>
           <Wrapper>
-
             <div style={{ flex: 1 }}>
-              <img src={book.bookcover} alt={book.title} style={{ width: '30rem', height: '35rem' }} />
+              <img
+                src={book.bookcover}
+                alt={book.title}
+                style={{ width: "30rem", height: "35rem" }}
+              />
             </div>
-            <div style={{ flex: 2, padding: '10px 40px 10px 40px' }}>
+            <div style={{ flex: 2, padding: "10px 40px 10px 40px" }}>
               <h2>{book.title}</h2>
               <h5>{book.writer} 작가</h5>
               <br />
               {/* <p>{book.summary}</p> */}
-              <p dangerouslySetInnerHTML={{ __html: book.summary.replace(/\n/g, '<br>') }} />
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: book.summary.replace(/\n/g, "<br>"),
+                }}
+              />
 
-              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '100px' }}>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  marginTop: "100px",
+                }}
+              >
                 <Button
                   variant="outline-primary"
                   className="custom-button-style"
-                  onClick={() => console.log('첫 번째 버튼 클릭')}
+                  onClick={handlePrint}
                 >
                   캐릭터 출력하기
                 </Button>
                 <Button
                   variant="outline-primary"
                   className="custom-button-style"
-                  onClick={() => console.log('두 번째 버튼 클릭')}
+                  onClick={() => console.log("두 번째 버튼 클릭")}
                 >
                   시작하기
                 </Button>
               </div>
             </div>
-      </Wrapper>
-          </Modal.Body>
-        </Modal>
+          </Wrapper>
+        </Modal.Body>
+      </Modal>
     </>
   );
 }
