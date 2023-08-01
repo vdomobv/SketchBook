eff68cf4b76de05e6c9c8169574e7d3472a489f8
vdomobv <vdomobv@naver.com>
2023-08-01 11:14:23 +0900

    <feat>[#300-LJE] check 페이지 틀 크기 조정

--- front-end/src/pages/Check/styles.jsx
+++ front-end/src/pages/Check/styles.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
   text-align: center;
+  margin : 0 auto;
 
   .container {
     padding: 100px;
@@ -21,7 +22,10 @@ const Wrapper = styled.div`
     border : 1px solid black;
     padding : 10px;
     display: flex;
-    
+    height : 600px;
+    border-radius: 10px;
+    border : 1px solid black;
+    width : 100%;
   }
 `;
 
