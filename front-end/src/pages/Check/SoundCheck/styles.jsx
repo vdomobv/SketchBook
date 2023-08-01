842d0c7f6cd0eb01e447d27e158c51ee34e63e2c
vdomobv <vdomobv@naver.com>
2023-08-01 15:11:33 +0900

    <feat>[#300-LJE] 음성에 따라 움직이는 바 구현 - 바 게이지 위에 이미지 띄우기 - 소리 끄기 버튼 클릭시, 0으로 설정

--- front-end/src/pages/Check/SoundCheck/styles.jsx
+++ front-end/src/pages/Check/SoundCheck/styles.jsx
@@ -1,5 +1,50 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const SoundWrapper = styled.div`
+padding : 30px;
+
+.box{
+    padding : 60px;
+    box-shadow: 1px 2px 10px grey;
+    border : none;
+}
+
+.sound{
+    padding : 20px;
+    width : 100%;
+    display: flex;
+    flex-direction: column;
+}
+
+.sound img { 
+    width: 100%;
+    height: 100%;
+    z-index: 2;
+}
+
+.gauge{
+    max-width: 100%;
+    height: 48%;
+    background-color: black;
+    position: absolute;
+    z-index: 1;
+}
+
+.volume {
+    width : 30%;
+    object-fit: contain;
+    z-index: 2;
+}
+
+button{
+    border : none;
+    border-radius : 10px;
+    font-weight : bold;
+    padding : 20px;
+    background-color : #D3B2FF;
+}
+
 
 `;
+
+export default SoundWrapper;
