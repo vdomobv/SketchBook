037ef529e5469b92e19da13c5f5488b201dc51b6
eazsilver <vdomobv@naver.com>
2023-08-02 21:31:27 +0900

    <feat>[#301-LJE] 로그인 안하면 페이지 url 직접 접속 차단

--- front-end/src/utils/isLogin.js
+++ front-end/src/utils/isLogin.js
@@ -1,4 +1,7 @@
 import Cookies from 'js-cookie';
 
-const isLogin = () =>  !!Cookies.get('x_auth')
+const isLogin = () =>  {
+    const userid = Cookies.get('x_auth')
+    return userid
+}
 export default isLogin;
