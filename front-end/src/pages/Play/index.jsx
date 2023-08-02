20f5a9deb56fea4f5a5ab49e7e9d9be8de7ab1c4
김진주 <zizu4256@naver.com>
2023-08-02 11:55:22 +0900

    <feat>[#313-KJJ] 동화 페이지 크기 전체화면으로 변경
    - 페이지 넘버링 위치 변경 (이미지 우측 하단)
    - 키보드 방향키로 동화 페이지 이동

--- front-end/src/pages/Play/index.jsx
+++ front-end/src/pages/Play/index.jsx
@@ -6,26 +6,26 @@ const imagesCount = 17; // 이미지의 총 개수
 function Play() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  // useCallback으로 handleNextImage 함수 감싸기
+  // 이전 페이지로 이동
   const handleNextImage = useCallback(() => {
     if (currentImageIndex === imagesCount - 1) {
-      // 마지막 이미지일 경우
       alert('마지막 페이지에요.');
     } else {
       setCurrentImageIndex((prevIndex) => prevIndex + 1);
     }
   }, [currentImageIndex]);
 
-  // useCallback으로 handlePrevImage 함수 감싸기
+  // 다음 페이지로 이동
   const handlePrevImage = useCallback(() => {
     if (currentImageIndex === 0) {
-      // 첫 번째 이미지일 경우
       alert('첫 페이지에요.');
     } else {
       setCurrentImageIndex((prevIndex) => prevIndex - 1);
     }
   }, [currentImageIndex]);
 
+  // 이미지 경로 저장
+  // currentImageIndex에 따라 동적으로 변하면서, 해당하는 이미지 파일을 불러와서 보여줄 수 있도록
   const imageSource = `./assets/play-background/엄마는카멜레온_${currentImageIndex}.jpg`;
 
   // 전역으로 keydown 이벤트를 감지하여 이미지 이동 처리
@@ -44,36 +44,24 @@ function Play() {
     };
   }, [currentImageIndex, handleNextImage, handlePrevImage]);
 
-  // Full Screen 모드로 전환하는 함수
-  const enterFullScreen = () => {
-    const element = document.documentElement; // HTML 요소를 가져옴
-
-    // Full Screen API를 사용하여 전체 화면으로 전환
-    if (element.requestFullscreen) {
-      element.requestFullscreen();
-    } else if (element.mozRequestFullScreen) {
-      element.mozRequestFullScreen();
-    } else if (element.webkitRequestFullscreen) {
-      element.webkitRequestFullscreen();
-    } else if (element.msRequestFullscreen) {
-      element.msRequestFullscreen();
-    }
-  };
 
   return (
     <Wrapper>
-      <div className="image-container">
-        <img src={imageSource} alt={`엄마는카멜레온_${currentImageIndex}`} />
-      </div>
-      <div className='numbering'>
+      <div className="image-wrap">
+        <div className="image-container">
+          <img src={imageSource} alt={`엄마는카멜레온_${currentImageIndex}`} />
+        </div>
+        {/* 페이지 번호 표시 (현 페이지 / 전체 페이지) */}
+        <div className='numbering' >{currentImageIndex + 1}/{imagesCount}</div>
+        {/* 페이지 이동 화살표 */}
+        {/* <div className='arrow-set'>
         <div className="arrow" onClick={handlePrevImage}>
           &larr;
         </div>
-        <div>{currentImageIndex + 1}/{imagesCount}</div>
         <div className="arrow" onClick={handleNextImage}>
           &rarr;
         </div>
-        <button onClick={enterFullScreen}>전체 화면 보기</button> {/* 전체 화면 보기 버튼 */}
+        </div> */}
       </div>
     </Wrapper>
   );
--- front-end/src/pages/Play/styles.jsx
+++ front-end/src/pages/Play/styles.jsx
@@ -4,7 +4,6 @@ const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh; /* 화면 세로 전체 높이에 맞게 설정합니다. */  
 
   .image-container {
     flex: 1;
@@ -14,23 +13,32 @@ const Wrapper = styled.div`
   }
 
   img {
-    max-width: 100%;
-    max-height: 100%;
-    object-fit: contain; /* 이미지가 비율을 유지한 채로 화면에 맞게 표시됩니다. */
+    objectFit: cover;
+    height: 100vh;
+    width: 100vw;
   }
 
   .numbering {
     display: flex;
     align-items: center;
-    // font-size: 100px;
-    // font-weight: 700;
+    justify-content: right;
+    position: absolute; 
+    right: 15px; 
+    bottom: 10px;
+    font-size: 15px;
   }
 
-  .arrow {
-    cursor: pointer;
-    font-size: 30px;
-    margin: 0 20px 0 20px;
-  }
+  // .arrow-set {
+  //   display: flex;
+  //   flex-direction: row;
+  //   justify-content: center;
+  // }
+
+  // .arrow {
+  //   cursor: pointer;
+  //   font-size: 30px;
+  //   margin: 0 20px 0 20px;
+  // }
 `;
 
 export default Wrapper;
