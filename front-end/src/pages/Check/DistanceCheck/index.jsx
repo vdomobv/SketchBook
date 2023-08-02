02f6514c35956cca89c616539324331b4941e81c
vdomobv <vdomobv@naver.com>
2023-08-02 14:00:52 +0900

    <feat>[#298-LJE] 페이지 별 타이틀 수정

--- front-end/src/pages/Check/DistanceCheck/index.jsx
+++ front-end/src/pages/Check/DistanceCheck/index.jsx
@@ -1,7 +1,7 @@
 function DistanceCheck() {
   return (
     <div>
-      <h2> 2. 거리 확인하기 </h2>
+      <h2> 2. 카메라 위치 확인하기 </h2>
       <iframe
         title="라즈베리파이 캠"
         src="http://192.168.100.246:8080/"
