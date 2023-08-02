d82a8008c21e48385f75c301163c6b08980c4bbf
vdomobv <vdomobv@naver.com>
2023-08-02 13:53:12 +0900

    <feat>[#298-LJE] 기기 연결 가이드 화살표 생성, 순서 연결

--- front-end/src/pages/Check/CaptureCheck/styles.jsx
+++ front-end/src/pages/Check/CaptureCheck/styles.jsx
@@ -4,11 +4,6 @@ import styled from "styled-components";
 const Wrapper = styled.div`
 font-family: 'Pretendard-Regular';
 justify-content: center;
-
-h2 {
-    font-size: 50px;
-    margin-top: 20px;
-}
 `;
 
 export default Wrapper;
