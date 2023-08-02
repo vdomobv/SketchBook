b70df10413e88baeec3aa1eb6a36bc805e6ef9c2
vdomobv <vdomobv@naver.com>
2023-08-02 11:10:24 +0900

    <feat>[#298-LJE] redux SoundCheck페이지 연결

--- front-end/src/pages/Check/Ready/index.jsx
+++ front-end/src/pages/Check/Ready/index.jsx
@@ -1,9 +1,19 @@
-function Ready() {
+import { connect } from "react-redux";
+
+function Ready(state) {
+  console.log(state);
+
   return (
     <div>
-      <h1>Ready</h1>
+      <h1>{state}</h1>
     </div>
   );
 }
 
-export default Ready;
+const mapStateToProps = (state) => {
+  return {
+    sound: state.device.sound,
+  };
+};
+
+export default connect(mapStateToProps)(Ready);
