4732b4724809b5684e501e7a6f4d9bbd6843aeae
안민 <anmin0517@gmail.com>
2023-08-02 14:26:30 +0900

    <feat>[#260-AM] 회원가입 시 이메일 인증, 로그아웃(아직 미완)

--- back-end/routers/usersRouter.js
+++ back-end/routers/usersRouter.js
@@ -4,6 +4,8 @@ const cookieParser = require("cookie-parser");
 
 const usersRouter = Router();
 
+const { Auth } = require("../middlewares/auth.js");
+
 usersRouter.use(bodyParser.urlencoded({ extended: true }));
 usersRouter.use(cookieParser());
 
@@ -13,7 +15,8 @@ usersRouter.route('/register').post(UsersDB.register);
 usersRouter.route('/idCheck').post(UsersDB.idCheck);
 usersRouter.route('/login').post(UsersDB.login);
 usersRouter.route('/auth').get(UsersDB.auth);
-usersRouter.route('/logout').get(UsersDB.logout);
+usersRouter.route('/logout').get(Auth, UsersDB.logout);
 usersRouter.route('/mail').post(UsersDB.mail);
+usersRouter.route('/checkVerificationCode').post(UsersDB.checkVerificationCode);
 
 module.exports = usersRouter;
