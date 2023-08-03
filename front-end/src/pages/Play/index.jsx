778989e9fec14a09824ada2337b068ed253117dd
김진주 <zizu4256@naver.com>
2023-08-03 13:33:42 +0900

    <feat>[#314-KJJ] 동화 페이지 전체보기, png 이미지 띄우기, 페이지 넘버링

--- front-end/src/pages/Play/index.jsx
+++ front-end/src/pages/Play/index.jsx
@@ -5,6 +5,7 @@ const imagesCount = 17; // 이미지의 총 개수
 
 function Play() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [hasPngImage, setHasPngImage] = useState(true);
 
   // 이전 페이지로 이동
   const handleNextImage = useCallback(() => {
@@ -24,10 +25,6 @@ function Play() {
     }
   }, [currentImageIndex]);
 
-  // 이미지 경로 저장
-  // currentImageIndex에 따라 동적으로 변하면서, 해당하는 이미지 파일을 불러와서 보여줄 수 있도록
-  const imageSource = `./assets/play-background/엄마는카멜레온_${currentImageIndex}.jpg`;
-
   // 전역으로 keydown 이벤트를 감지하여 이미지 이동 처리
   useEffect(() => {
     const handleKeyDown = (e) => {
@@ -42,26 +39,34 @@ function Play() {
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  }, [currentImageIndex, handleNextImage, handlePrevImage]);
+  }, [handleNextImage, handlePrevImage]);
 
+  const backgroundImageSource = `./assets/play-background/엄마는카멜레온_${currentImageIndex}.jpg`;
+  const pngImageSource = `./assets/play-png/${currentImageIndex}_엄마는카멜레온.png`;
+
+  useEffect(() => {
+    // png 이미지의 존재 여부를 체크합니다.
+    const img = new Image();
+    img.onload = () => {
+      setHasPngImage(true);
+    };
+    img.onerror = () => {
+      setHasPngImage(false);
+    };
+    img.src = pngImageSource;
+  }, [pngImageSource]);
 
   return (
     <Wrapper>
-      <div className="image-wrap">
+      <div>
         <div className="image-container">
-          <img src={imageSource} alt={`엄마는카멜레온_${currentImageIndex}`} />
+          <img src={backgroundImageSource} alt={`엄마는카멜레온_${currentImageIndex}`} />
+          {hasPngImage && (
+            <img className="png-image" src={pngImageSource} alt={`엄마는카멜레온_${currentImageIndex}`} />
+          )}
         </div>
         {/* 페이지 번호 표시 (현 페이지 / 전체 페이지) */}
         <div className='numbering' >{currentImageIndex + 1}/{imagesCount}</div>
-        {/* 페이지 이동 화살표 */}
-        {/* <div className='arrow-set'>
-        <div className="arrow" onClick={handlePrevImage}>
-          &larr;
-        </div>
-        <div className="arrow" onClick={handleNextImage}>
-          &rarr;
-        </div>
-        </div> */}
       </div>
     </Wrapper>
   );
