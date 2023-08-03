ef40202e0b60ac470f93218f306d8f390a77afc7
서정빈 <jeongb9716@gmail.com>
2023-08-03 11:47:13 +0900

    <feat>[#288-SJB] 기기 OTP 연결

--- back-end/controllers/deviceDB.js
+++ back-end/controllers/deviceDB.js
@@ -1,25 +1,42 @@
 const { Device } = require("../models/device");
 const { client } = require("../server.js");
-const otpGenerator = require('otp-generator');
+const otpGenerator = require("otp-generator");
 
-function issue(req, res) {    
+function issue(req, res) {
+  const { email } = req.user;
 
-    const { email } = req.user;
+  // Generate a random OTP using the otp-generator package
+  const otp = otpGenerator.generate(6, {
+    lowerCaseAlphabets: false,
+    upperCaseAlphabets: false,
+    specialChars: false,
+  });
 
-    // Generate a random OTP using the otp-generator package
-    const otp = otpGenerator.generate(6, {
-      lowerCaseAlphabets: false,
-      upperCaseAlphabets: false,
-      specialChars: false,
+  client.set(otp, email);
+  client.expire(otp, 200); // 입력시간을 고려하여 3분 20초 설정
+
+  res.status(200).json({
+    email: email,
+    otp: otp,
+  });
+}
+
+function connect(req, res) {
+  const otp = req.body.otp;
+
+  client.on(otp, (err, value) => {
+    if (err) {
+      return res.json({
+        message: "해당 OTP가 존재하지 않습니다.",
+      });
+    }
+
+    return res.status(200).json({
+      loginSuccess: true,
+      useremail: value,
     });
-
-    client.set(otp, email);
-    client.expire(otp, 200); // 입력시간을 고려하여 3분 20초 설정
-
-    res.status(200).json({
-        email: email,
-        otp: otp,
-      })
+  });
 }
 
 exports.issue = issue;
+exports.connect = connect;
