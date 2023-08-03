1026f45b1992177ca64cf0224e85b1bae2cd26e4
서정빈 <jeongb9716@gmail.com>
2023-08-03 09:23:54 +0900

    <feat>[#219-SJB] OTP 생성

--- back-end/routers/usersRouter.js
+++ back-end/routers/usersRouter.js
@@ -5,12 +5,12 @@ const usersRouter = Router();
 const UsersDB = require("../controllers/usersDB.js");
 const { Auth } = require("../middlewares/auth.js");
 
-usersRouter.route('/register').post(UsersDB.register);
-usersRouter.route('/idCheck').post(UsersDB.idCheck);
-usersRouter.route('/login').post(UsersDB.login);
-usersRouter.route('/auth').get(Auth, UsersDB.auth);
-usersRouter.route('/logout').get(Auth, UsersDB.logout);
-usersRouter.route('/mail').post(UsersDB.mail);
-usersRouter.route('/checkVerificationCode').post(UsersDB.checkVerificationCode);
+usersRouter.route('/register').post(UsersDB.register); //
+usersRouter.route('/idCheck').post(UsersDB.idCheck); //
+usersRouter.route('/login').post(UsersDB.login); //
+usersRouter.route('/auth').get(Auth, UsersDB.auth); //
+usersRouter.route('/logout').get(Auth, UsersDB.logout); //
+usersRouter.route('/mail').post(UsersDB.mail); //
+usersRouter.route('/checkVerificationCode').post(UsersDB.checkVerificationCode); // 
 
 module.exports = usersRouter;
