89f6d6011886c1fe761d4e640e3436e1825376e7
jochanik <whcksdlr5909@naver.com>
2023-08-04 15:06:04 +0900

    <feat>{#333-JCI] 동화 16, 17 페이지 구현 및 CSS 추가

---
+++ front-end/src/pages/Play/Story1/P16/index.jsx
@@ -0,0 +1,13 @@
+import Wrapper from "./styles";
+import image16 from '../../../../play-background/엄마는카멜레온_16.jpg';
+
+function P1() {
+
+    return (
+        <Wrapper>
+            <img src={image16} alt="" /> 
+        </Wrapper>
+    )
+}
+
+export default P1;
---
+++ front-end/src/pages/Play/Story1/P17/index.jsx
@@ -0,0 +1,13 @@
+import Wrapper from "./styles";
+import image17 from '../../../../play-background/엄마는카멜레온_17.jpg';
+
+function P1() {
+
+    return (
+        <Wrapper>
+            <img src={image17} alt="" />
+        </Wrapper>
+    )
+}
+
+export default P1;
