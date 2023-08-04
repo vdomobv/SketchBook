de1c60461dfed9d2afac02c6275d939048c56841
김진주 <zizu4256@naver.com>
2023-08-04 15:00:22 +0900

    <feat>[#000-KJJ] 동화 페이지 P6-10 추가 및 App.js 수정

---
+++ front-end/src/pages/Play/Story1/P6/index.jsx
@@ -0,0 +1,14 @@
+import Wrapper from "./styles";
+import image1 from '../../../../play-background/엄마는카멜레온_6.jpg';
+
+function P6() {
+
+    return (
+        <Wrapper>
+            <img src={image1} alt="" />
+            
+        </Wrapper>
+    )
+}
+
+export default P6;
