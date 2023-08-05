a45724f150d9f6d1f4884c01c2e3217fe39b3911
김진주 <zizu4256@naver.com>
2023-08-05 10:28:29 +0900

    [#315-KJJ] story1 동화 페이지 방향키로 이동 구현
    - Play/story1/p1 ~ p17
    - story1의 index.jsx에 페이지 이동 로직 구현
    
    
    Former-commit-id: 477594e90fb72fc48cd44c40de3255973c4d547d

--- front-end/src/pages/Play/Story1/index.jsx
+++ front-end/src/pages/Play/Story1/index.jsx
@@ -1,9 +1,37 @@
 import { Outlet } from 'react-router';
-
-
+import { useEffect } from 'react';
 
 function Story1() {
-  
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      // 페이지 이동 로직 구현
+      const currentPath = window.location.pathname;
+      const pageNumber = Number(currentPath.split('/').pop().slice(1));
+      let nextPageNumber;
+
+      if (event.key === 'ArrowLeft') {
+        nextPageNumber = Math.max(1, pageNumber - 1);
+      } else if (event.key === 'ArrowRight') {
+        nextPageNumber = Math.min(17, pageNumber + 1);
+      } else {
+        return;
+      }
+
+      // 이동할 페이지 경로 생성
+      const nextPagePath = `/Play/story1/p${nextPageNumber}`;
+
+      // 페이지 이동
+      window.location.href = nextPagePath;
+    };
+
+    // 이벤트 리스너 추가
+    window.addEventListener('keydown', handleKeyDown);
+
+    // 컴포넌트 언마운트 시 이벤트 리스너 제거
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
 
   return (
     <div>
