6a60ac42e6b78677e4a197957950e1552badcb02
안민 <anmin0517@gmail.com>
2023-08-06 17:35:27 +0900

    <feat>[#330-AM] 나레이션 삽입

--- front-end/src/pages/Play/Story1/P15/index.jsx
+++ front-end/src/pages/Play/Story1/P15/index.jsx
@@ -1,11 +1,15 @@
 import Wrapper from "./styles";
 import image from '../../../../play-background/엄마는 카멜레온_15.gif';
+import audio15 from '../../../../play-background/ske_15.mp3';
 
 function P15() {
 
     return (
         <Wrapper>
             <img src={image} alt="" />
+            <audio autoPlay>
+                <source src={audio15} type="audio/mp3" />
+            </audio>
         </Wrapper>
     )
 }
--- front-end/src/pages/Play/Story1/P7/index.jsx
+++ front-end/src/pages/Play/Story1/P7/index.jsx
@@ -1,14 +1,17 @@
 import Wrapper from "./styles";
 import image1 from '../../../../play-background/엄마는 카멜레온_7.gif';
+import audio7 from '../../../../play-background/ske_7.mp3';
 
-function P5() {
+function P7() {
 
     return (
         <Wrapper>
             <img src={image1} alt="" />
-            
+            <audio autoPlay>
+                <source src={audio7} type="audio/mp3" />
+            </audio>            
         </Wrapper>
     )
 }
 
-export default P5;
+export default P7;
