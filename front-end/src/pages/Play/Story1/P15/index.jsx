a698a9c81a33c5bfd06e6d47a8c510d871b0c3d8
김진주 <zizu4256@naver.com>
2023-08-06 17:53:52 +0900

    <feat>[#315-KJJ] play1 페이지 이동 navigate
    - 1페이지, 17페이지 '마지막 페이지' alert 및 책장 이동

--- front-end/src/pages/Play/Story1/P15/index.jsx
+++ front-end/src/pages/Play/Story1/P15/index.jsx
@@ -1,13 +1,13 @@
 import Wrapper from "./styles";
-import image from '../../../../play-background/엄마는 카멜레온_15.gif';
+import image15 from '../../../../play-background/엄마는 카멜레온_15.gif';
 
-function P15() {
+function P1() {
 
     return (
         <Wrapper>
-            <img src={image} alt="" />
+            <img src={image15} alt="" /> 
         </Wrapper>
     )
 }
 
-export default P15;
+export default P1;
--- front-end/src/pages/Play/Story1/P16/index.jsx
+++ front-end/src/pages/Play/Story1/P16/index.jsx
@@ -1,13 +1,34 @@
+import image from '../../../../play-background/엄마는 카멜레온_16.gif';
+// import { useState, useEffect } from 'react';
+// import Image16 from '../../../../play-png/16_엄마손.png';
 import Wrapper from "./styles";
-import image16 from '../../../../play-background/엄마는 카멜레온_16.gif';
 
-function P1() {
+function P15() {
+    // const [isAnimationStarted, setIsAnimationStarted] = useState(false);
+
+    // useEffect(() => {
+    //     // 3초 뒤에 애니메이션 시작을 표시합니다.
+    //     const timer = setTimeout(() => {
+    //         setIsAnimationStarted(true);
+    //     }, 2700000);
+
+    //     return () => {
+    //         clearTimeout(timer);
+    //     };
+    // }, []);
 
     return (
         <Wrapper>
-            <img src={image16} alt="" /> 
+            <div>
+                <img src={image} alt="" />
+                {/* <img
+                    className={isAnimationStarted ? 'png-image fadeIn' : 'png-image'}
+                    src={Image16}
+                    alt="Image 16"
+                /> */}
+            </div>
         </Wrapper>
-    )
+    );
 }
 
-export default P1;
+export default P15;
