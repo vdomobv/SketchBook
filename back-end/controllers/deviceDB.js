6671d74b5eca36827e6fd6f0a45d37884b010321
eazsilver <vdomobv@naver.com>
2023-08-06 15:09:55 +0900

    <bug>[#000-LJE] 다른 번호로 기기 연결되는 문제 해결
    Former-commit-id: a5422140a13df69c6461116632483306cb0c4b92

--- back-end/controllers/deviceDB.js
+++ back-end/controllers/deviceDB.js
@@ -14,7 +14,7 @@ function issue(req, res) {
   });
 
   client.set(otp, email);
-  client.expire(otp, 2000); // 입력시간을 고려하여 3분 20초 설정
+  client.expire(otp, 200); // 입력시간을 고려하여 3분 20초 설정
 
   res.status(200).json({
     email: email,
