eb9bfb3f95aa6e2d7d3447940a9a483768889569
김진주 <zizu4256@naver.com>
2023-08-08 11:25:10 +0900

    <feat>[#315-KJJ] 16페이지 엄마손 TEST

--- front-end/src/pages/Play/Story1/P16/styles.jsx
+++ front-end/src/pages/Play/Story1/P16/styles.jsx
@@ -1,11 +1,30 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .image-container {
+    position: relative;
+  }
 
   img {
+    width: 100%;
     background-size: cover;
   }
 
+  .image-button {
+    position: absolute;
+    top: 50%; /* 이미지 중앙에 위치 */
+    right: 0;
+    transform: translateY(-50%);
+    padding: 10px 20px;
+    background-color: #3498db;
+    color: white;
+    border: none;
+    cursor: pointer;
+  }
 `;
 
 export default Wrapper;
