62b243caf13fcda8e531dfd81d22fcb2075d6b08
김진주 <zizu4256@naver.com>
2023-08-08 17:12:42 +0900

    <feat>[#315-KJJ] story1 페이지 alert창 커스텀 모달로 변경

--- front-end/src/pages/Check/CombineCheck/index.jsx
+++ front-end/src/pages/Check/CombineCheck/index.jsx
@@ -16,7 +16,7 @@ function CombineCheck() {
         <h3 style={{ fontSize: '45px', fontWeight: 'bold' }}> 5. 캐릭터 연동 확인하기 </h3>
         <iframe
           title="라즈베리파이 캠"
-          src="http://192.168.100.246:8080/"
+          // src="http://192.168.100.246:8080/"
           width="100%"
           height="800px"
         ></iframe>
