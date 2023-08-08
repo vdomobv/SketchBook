197b2afd832522f896de15a76bd4df7cb23d7181
김진주 <zizu4256@naver.com>
2023-08-08 17:12:42 +0900

    <feat>[#315-KJJ] story1 페이지 alert창 커스텀 모달로 변경
    Former-commit-id: 7ce1aff1ad917fc5c9ac4fbbfe7cd4d02532d479

--- front-end/src/pages/Check/MotionCheck/index.jsx
+++ front-end/src/pages/Check/MotionCheck/index.jsx
@@ -16,7 +16,7 @@ function MotionCheck() {
         <h3 style={{ fontSize: '45px', fontWeight: 'bold' }}> 3. 움직임 확인하기 </h3>
         <iframe
           title="라즈베리파이 캠"
-          src="http://192.168.100.246:8080/"
+          // src="http://192.168.100.246:8080/"
           width="100%"
           height="800px"
         ></iframe>
