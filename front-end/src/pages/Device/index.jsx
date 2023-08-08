184e0e14e32dd53677c24454f9ac2ffc0730dec5
안민 <anmin0517@gmail.com>
2023-08-08 15:01:04 +0900

    <feat>[#330-AM] ABOUTUS 헤더 수정중

--- front-end/src/pages/Device/index.jsx
+++ front-end/src/pages/Device/index.jsx
@@ -10,7 +10,7 @@ function Device() {
       <h2 style={{display:'flex', justifyContent: 'center'}}> 기기가 연결되었어요! </h2>
       <iframe
         title="라즈베리파이 캠"
-        src="http://192.168.100.246:8080/"
+        // src="http://192.168.100.246:8080/"
         width="100%"
         height="800px"
       ></iframe>
