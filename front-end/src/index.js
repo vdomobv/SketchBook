22c7f5d4b4ea55c97299ce7bbce7720030e01048
jochanik <whcksdlr5909@naver.com>
2023-08-08 14:09:18 +0900

    <style>[000-JCI]불필요한 콘솔창 주석처리
    - FrontEnd 폴더
    - BackEnd 폴더
    - 불필요한 console.log 창 주석 및 삭제 처리
    
    
    Former-commit-id: d6a09c123282b593ced3126755de5a2695267e52

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -9,7 +9,6 @@ import rootReducer from './redux/modules';
 import { configureStore } from '@reduxjs/toolkit'
 
 const store = configureStore({ reducer: rootReducer })
-console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
