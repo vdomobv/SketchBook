1a3930d9fbb4dab50b94720600759a2c64c30a4d
안민 <anmin0517@gmail.com>
2023-08-09 11:26:09 +0900

    <feat>[#330-AM] 나레이션 추가
    Former-commit-id: a821cad578ebf51572901ad043e689901692f3d7

--- front-end/src/pages/Play/Story1/P7/index.jsx
+++ front-end/src/pages/Play/Story1/P7/index.jsx
@@ -1,14 +1,8 @@
 import Wrapper from "./styles";
 import image1 from '../../../../play-background/엄마는 카멜레온_7.gif';
 import audio7 from '../../../../play-background/ske_7.mp3';
-import { useNavigate } from 'react-router-dom';
 
 function P7() {
-    const navigate = useNavigate();
-        setTimeout(() => {
-            navigate('/play/story1/p8');
-        }, 15000);
-
     return (
         <Wrapper>
             <img src={image1} alt="" />
