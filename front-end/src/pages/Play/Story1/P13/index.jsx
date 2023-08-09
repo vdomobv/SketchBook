b0937f9c7e6503905a6aa74595b3191005100775
vdomobv <vdomobv@naver.com>
2023-08-09 09:36:45 +0900

    <bug>[#000-LJE] 나레이션 끝나면 페이지 이동하도록 타이머 추가 11-15페이지
    Former-commit-id: ed3d2f3e10522306d103488c6ede8d55002a67bf

--- front-end/src/pages/Play/Story1/P13/index.jsx
+++ front-end/src/pages/Play/Story1/P13/index.jsx
@@ -1,9 +1,14 @@
 import Wrapper from "./styles";
 import image from '../../../../play-background/엄마는 카멜레온_13.gif';
 import audio13 from '../../../../play-background/ske_13.mp3';
-
+import { useNavigate } from "react-router";
 
 function P13() {
+    const navigate = useNavigate();
+    setTimeout(() => {
+        navigate('/play/story1/p14');
+      }, 8000);
+
 
     return (
         <Wrapper>
--- front-end/src/pages/Play/Story1/P14/index.jsx
+++ front-end/src/pages/Play/Story1/P14/index.jsx
@@ -1,9 +1,13 @@
 import Wrapper from "./styles";
 import image from '../../../../play-background/엄마는 카멜레온_14.gif';
 import audio14 from '../../../../play-background/ske_14.mp3';
-
+import { useNavigate } from "react-router";
 
 function P14() {
+    const navigate = useNavigate();
+    setTimeout(() => {
+        navigate('/play/story1/p15');
+      }, 8000);
 
     return (
         <Wrapper>
