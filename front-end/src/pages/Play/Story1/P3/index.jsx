0018fbbc9bb296d35dec7abca0b0178f05d69c5f
서정빈 <jeongb9716@gmail.com>
2023-08-09 20:57:33 +0900

    <style>[#225-SJB] 불피요한 console.log 삭제 및 코드 통일성 확보

--- front-end/src/pages/Play/Story1/P3/index.jsx
+++ front-end/src/pages/Play/Story1/P3/index.jsx
@@ -1,45 +1,43 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Wrapper from "./styles";
-import image1 from '../../../../play-background/엄마는 카멜레온_3.gif';
-import audio3 from '../../../../play-background/ske_3.mp3';
+import image1 from "../../../../play-background/엄마는 카멜레온_3.gif";
+import audio3 from "../../../../play-background/ske_3.mp3";
 import axios from "axios";
 
 function P3() {
   let navigate = useNavigate();
 
-  const mission = (e) => {
-      
-    axios
-      .post("/api/devices/mission", {
-        flag: "0" // mission이 없으면 0 있으면 1
-      })
-      .then((res) => {
-        console.log(res.data.mission)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-    }
-
-    useEffect(() => {
+  useEffect(() => {
     const timer = setTimeout(() => {
-      navigate('/Play/story1/P4'); 
+      navigate("/Play/story1/P4");
     }, 19000);
 
     return () => clearTimeout(timer); // 컴포넌트가 언마운트되면 타이머 취소
   }, []); // 빈 의존성 배열을 사용하여 마운트 시에만 타이머 설정
 
-    return (
-        <Wrapper onLoad={mission}>
-            <img src={image1} alt="" />
+  const mission = (e) => {
+    axios
+      .post("/api/devices/mission", {
+        flag: "0", // mission이 없으면 0 있으면 1
+      })
+      .then((res) => {
+        // console.log(res.data.mission);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-            <audio autoPlay>
-                <source src={audio3} type="audio/mp3" />
-            </audio>
-            
-        </Wrapper>
-    )
+  return (
+    <Wrapper onLoad={mission}>
+      <img src={image1} alt="" />
+
+      <audio autoPlay>
+        <source src={audio3} type="audio/mp3" />
+      </audio>
+    </Wrapper>
+  );
 }
 
 export default P3;
--- front-end/src/pages/Play/Story1/P5/index.jsx
+++ front-end/src/pages/Play/Story1/P5/index.jsx
@@ -1,45 +1,43 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Wrapper from "./styles";
-import image1 from '../../../../play-background/엄마는 카멜레온_5.gif';
-import audio5 from '../../../../play-background/ske_5.mp3';
+import image1 from "../../../../play-background/엄마는 카멜레온_5.gif";
+import audio5 from "../../../../play-background/ske_5.mp3";
 import axios from "axios";
 
 function P5() {
-    let navigate = useNavigate();
+  let navigate = useNavigate();
 
-    const mission = (e) => {
-      
-        axios
-          .post("/api/devices/mission", {
-            flag: "0" // mission이 없으면 0 있으면 1
-          })
-          .then((res) => {
-            console.log(res.data.mission)
-          })
-          .catch((err) => {
-            console.log(err)
-          })
-        }
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/Play/story1/P6");
+    }, 24000);
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            navigate('/Play/story1/P6');
-        }, 24000);
+    return () => clearTimeout(timer); // 컴포넌트가 언마운트되면 타이머 취소
+  }, []); // 빈 의존성 배열을 사용하여 마운트 시에만 타이머 설정
 
-        return () => clearTimeout(timer); // 컴포넌트가 언마운트되면 타이머 취소
-    }, []); // 빈 의존성 배열을 사용하여 마운트 시에만 타이머 설정
+  const mission = (e) => {
+    axios
+      .post("/api/devices/mission", {
+        flag: "0", // mission이 없으면 0 있으면 1
+      })
+      .then((res) => {
+        // console.log(res.data.mission);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    return (
-        <Wrapper onLoad={mission}>
-            <img src={image1} alt="" />
+  return (
+    <Wrapper onLoad={mission}>
+      <img src={image1} alt="" />
 
-            <audio autoPlay>
-                <source src={audio5} type="audio/mp3" />
-            </audio>
-
-        </Wrapper>
-    )
+      <audio autoPlay>
+        <source src={audio5} type="audio/mp3" />
+      </audio>
+    </Wrapper>
+  );
 }
 
 export default P5;
