5549dd6beb15c9a511fdb5045950abe484325bb4
김진주 <zizu4256@naver.com>
2023-08-09 09:58:47 +0900

    <feat>[#345-KJJ] 나레이션 타임아웃 설정
    - story1 페이지 p6~p10

--- front-end/src/pages/Play/Story1/P7/index.jsx
+++ front-end/src/pages/Play/Story1/P7/index.jsx
@@ -1,8 +1,13 @@
 import Wrapper from "./styles";
 import image1 from '../../../../play-background/엄마는 카멜레온_7.gif';
 import audio7 from '../../../../play-background/ske_7.mp3';
+import { useNavigate } from 'react-router-dom';
 
 function P7() {
+    const navigate = useNavigate();
+        setTimeout(() => {
+            navigate('/play/story1/p8');
+        }, 15000);
 
     return (
         <Wrapper>
--- front-end/src/pages/Play/Story1/P9/index.jsx
+++ front-end/src/pages/Play/Story1/P9/index.jsx
@@ -1,9 +1,13 @@
 import Wrapper from "./styles";
 import image1 from '../../../../play-background/엄마는 카멜레온_9.gif';
 import audio9 from '../../../../play-background/ske_9.mp3';
-
+import { useNavigate } from 'react-router-dom';
 
 function P9() {
+    const navigate = useNavigate();
+        setTimeout(() => {
+            navigate('/play/story1/p10');
+        }, 11000);
 
     return (
         <Wrapper>
