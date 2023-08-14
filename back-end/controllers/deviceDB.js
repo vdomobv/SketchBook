e9f90b05ce94d2f496055f96bbc32eb651d9fbd5
서정빈 <jeongb9716@gmail.com>
2023-08-14 11:39:24 +0900

    <test>[#225-SJB] 배포용 코드 변경
    Former-commit-id: 325994ac4d3a37c9b3d6cb070f4a1fb1742449b3

--- back-end/controllers/deviceDB.js
+++ back-end/controllers/deviceDB.js
@@ -147,7 +147,6 @@ async function downloadImage(url, filename, email) {
     // 이미지를 저장할 경로 설정 (현재 디렉토리 기준)
     // const imagePath = path.join('dir','..', '/user' ,email, filename); // local
     const imagePath = path.join("/server/user", email, filename); // 배포
-    // const imagePath = `/server/user/${email}/${filename}`; // 배포
 
     // 파일 저장
     fs.writeFileSync(imagePath, imageData);
