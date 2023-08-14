b208184e8fe2c14e121cd9cdf1867cd21beac383
서정빈 <jeongb9716@gmail.com>
2023-08-14 09:48:11 +0900

    <fix>[#225-SJB] redis저장 위치 변경
    Former-commit-id: 5422db1f28c9c696d7f8107ba07cd7e3a5711a9b

--- back-end/controllers/deviceDB.js
+++ back-end/controllers/deviceDB.js
@@ -2,13 +2,13 @@ const { User } = require("../models/users.js");
 const { Device } = require("../models/device");
 const { client } = require("../server.js");
 const otpGenerator = require("otp-generator");
-const fs = require('fs');
-const axios = require('axios');
-const path = require('path');
+const fs = require("fs");
+const axios = require("axios");
+const path = require("path");
 
 let OTP = "0000";
 
-function issue(req, res) {
+async function issue(req, res) {
   const { email } = req.user;
 
   // Generate a random OTP using the otp-generator package
@@ -20,7 +20,7 @@ function issue(req, res) {
 
   OTP = otp;
 
-  client.select(0);
+  await client.select(0);
   client.set(otp, email);
   client.expire(otp, 200); // 입력시간을 고려하여 3분 20초 설정
 
@@ -32,9 +32,9 @@ function issue(req, res) {
 
 async function checkConnect(req, res) {
   await client.select(1);
-  client.set(req.user.email, 'ready');
+  client.set(req.user.email, "ready");
   const flag = await client.get(OTP);
-  
+
   if (flag === "true") {
     User.findOneAndUpdate(
       { _id: req.user._id },
@@ -67,20 +67,23 @@ async function checkConnect(req, res) {
 }
 
 async function disconnect(req, res) {
-  await client.select(1)
+  await client.select(1);
   if (req.user.email == "connect@test.com") {
     res.status(200).json({
       success: true,
     });
   } else {
-    User.findOneAndUpdate( { _id: req.user._id }, { isConnected: false }, (err, user) => {
+    User.findOneAndUpdate(
+      { _id: req.user._id },
+      { isConnected: false },
+      (err, user) => {
         if (err) {
           return res.json({
             success: false,
             err,
           });
         }
-    client.set(req.user.email, 'logout');
+        client.set(req.user.email, "logout");
 
         res
           .clearCookie("isConnected")
@@ -97,7 +100,7 @@ async function disconnect(req, res) {
 async function start(req, res) {
   await client.select(1);
   // await client.RPUSHX("tst", "start");
-  await client.set(req.user.email,'start');
+  await client.set(req.user.email, "start");
   return res.status(200).json({});
 }
 
@@ -120,28 +123,30 @@ async function mission(req, res) {
 
   await client.select(1);
 
-  if (flag == "1") {
-    client.set(req.user.email, "mission");
-    return res.status(200).json({
-      email: req.user.email,
-      mission: true,
-    });
-  } else {
-    client.set(req.user.email, "story");
-    return res.status(200).json({
-      mission: false,
-    });
+  if (await client.TYPE(req.user.mail) !== "list") {
+    if (flag == "1") {
+      client.set(req.user.email, "mission");
+      return res.status(200).json({
+        email: req.user.email,
+        mission: true,
+      });
+    } else {
+      client.set(req.user.email, "story");
+      return res.status(200).json({
+        mission: false,
+      });
+    }
   }
 }
 
 async function downloadImage(url, filename, email) {
   try {
-    const response = await axios.get(url, { responseType: 'arraybuffer' });
-    const imageData = Buffer.from(response.data, 'binary');
+    const response = await axios.get(url, { responseType: "arraybuffer" });
+    const imageData = Buffer.from(response.data, "binary");
 
     // 이미지를 저장할 경로 설정 (현재 디렉토리 기준)
     // const imagePath = path.join('dir','..', '/user' ,email, filename); // local
-    const imagePath = path.join('/server/user' ,email, filename); // 배포
+    const imagePath = path.join("/server/user", email, filename); // 배포
     // const imagePath = `/server/user/${email}/${filename}`; // 배포
 
     // 파일 저장
@@ -149,7 +154,7 @@ async function downloadImage(url, filename, email) {
 
     console.log(`이미지가 ${imagePath}에 저장되었습니다.`);
   } catch (error) {
-    console.error('이미지 다운로드 에러:', error.message);
+    console.error("이미지 다운로드 에러:", error.message);
   }
 }
 
@@ -160,29 +165,28 @@ function capture(req, res) {
   console.log(imgUrl);
 
   // downloadImage("http://localhost:3000" + imgUrl, `character.png`, email) // local
-  downloadImage("https://i9c102.p.ssafy.io" + imgUrl, `character.png`, email) // 배포
+  downloadImage("https://i9c102.p.ssafy.io" + imgUrl, `character.png`, email); // 배포
 
   return res.status(200).json({
     download: "succes",
   });
 }
 
-async function position (req, res) {
+async function position(req, res) {
   const user = req.user.email;
 
   await client.select(4);
-  const type = await client.type(user)
+  const type = await client.type(user);
 
   let x_diff = 0;
   let y_diff = 0;
-  console.log(type)
+  console.log(type);
 
-  if(type === 'list') {
-    if(await client.LLEN(user) <= 2) {
+  if (type === "list") {
+    if ((await client.LLEN(user)) <= 2) {
       x_diff = 0;
       y_diff = 0;
-    }
-    else {
+    } else {
       let diff = await client.lRange(user, 0, 1);
       x_diff = diff[0];
       y_diff = diff[1];
@@ -196,8 +200,8 @@ async function position (req, res) {
   return res.status(200).json({
     email: user,
     x_diff: x_diff,
-    y_diff: y_diff
-  })
+    y_diff: y_diff,
+  });
 }
 
 exports.issue = issue;
