1c3aae63311deddbc3ec569dc7fb4769dcee5776
vdomobv <vdomobv@naver.com>
2023-08-14 10:42:07 +0900

    <bug>[#000-LJE] maximum update exceeded 에러 수정 - interval로 수정 시기 조정

--- front-end/src/pages/Play/Story1/P7/index.jsx
+++ front-end/src/pages/Play/Story1/P7/index.jsx
@@ -8,69 +8,89 @@ import audio7 from "../../../../play-background/ske_7.mp3";
 import axios from "axios";
 // import Livecam from "../../../../components/Livecam";
 import { useEffect, useLayoutEffect, useState } from "react";
+const Charactercam = (props) => {
+  const [characterUrl, setcharacterUrl] = useState();
+  const { setBottom, setLeft } = props;
 
-function P7() {  
+  let email;
+
+  useEffect(() => {
+    // 위치 정보 업데이트 함수
+    const updatePosition = () => {
+      axios
+        .get("/api/devices/position")
+        .then((res) => {
+          const email = res.data.email;
+          const x_diff = parseFloat(res.data.x_diff);
+          const y_diff = parseFloat(res.data.y_diff);
+
+          setBottom((prevBottom) => prevBottom + y_diff);
+          setLeft((prevLeft) => prevLeft + x_diff);
+        })
+        .catch((err) => {
+          return console.log("에러입니다.", err);
+        });
+    };
+
+    // 컴포넌트 마운트 시 처음 한 번 호출
+    updatePosition();
+
+    // 10초마다 API 호출
+    const interval = setInterval(updatePosition, 200);
+
+    // 컴포넌트 언마운트 시 인터벌 정리
+    return () => clearInterval(interval);
+  }, []);
+
+  const fetchNewImage = () => {
+    const timestamp = new Date().getTime();
+    setcharacterUrl(`/assets/assemble.png?timestamp=${timestamp}`); //local
+    // setcharacterUrl(`/user/${email}/assemble.png?timestamp=${timestamp}`); // 배포
+  };
+
+  useLayoutEffect(() => {
+    fetchNewImage(); // 컴포넌트가 마운트될 때 이미지 가져오기
+    const interval = setInterval(fetchNewImage, 10000); // 200ms마다 이미지 업데이트
+    return () => clearInterval(interval); // 컴포넌트 언마운트 시 인터벌 클리어
+  });
+
+  return <img src={characterUrl} alt="" />;
+};
+
+function P7() {
   const mission = (e) => {
     axios
-    .post("/api/devices/mission", {
-      flag: "1", // mission이 없으면 0 있으면 1
-    })
-    .then((res) => {
-      // console.log(res.data.mission);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+      .post("/api/devices/mission", {
+        flag: "1", // mission이 없으면 0 있으면 1
+      })
+      .then((res) => {
+        // console.log(res.data.mission);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
   const [bottom, setBottom] = useState(0);
   const [left, setLeft] = useState(0);
-  
-  const Charactercam = () => {
-    const [characterUrl, setcharacterUrl] = useState();
-    let email;
-    axios
-      .get("/api/devices/position")
-      .then((res) => {
-        email = res.data.email;
-        // 카메라 화면 : "user/[user_email]/image.jpg"
-        // 캐릭터 : user/[user_email]/assemble.png
-        const x_diff = parseFloat(res.data.x_diff);
-        const y_diff = parseFloat(res.data.y_diff);
-
-        setBottom((prevBottom) => prevBottom + y_diff);
-        setLeft((prevLeft) => prevLeft + x_diff);
-      })
-      .catch((err) => {
-        return console.log("에러입니다.", err);
-      });
-
-    const fetchNewImage = () => {
-      const timestamp = new Date().getTime();
-      setcharacterUrl(`/assets/assemble.png?timestamp=${timestamp}`); //local
-      // setcharacterUrl(`/user/${email}/assemble.png?timestamp=${timestamp}`); // 배포
-    };
-
-    useLayoutEffect(() => { 
-      fetchNewImage(); // 컴포넌트가 마운트될 때 이미지 가져오기
-      const interval = setInterval(fetchNewImage, 10000); // 200ms마다 이미지 업데이트
-      return () => clearInterval(interval); // 컴포넌트 언마운트 시 인터벌 클리어
-    });
-
-    console.log(left);
-
-    return <img src={characterUrl} alt="" />;
-  };
 
   return (
-    <Wrapper >
-      <img className="back-ground" src={image1} alt="" onLoad={mission}/>      
-      <div className="character-cam" style={{ left: `${left}px`, bottom: `${bottom}px`, position: "absolute", zIndex: 1 }} >
-        <Charactercam />
+    <Wrapper>
+      <img className="back-ground" src={image1} alt="" onLoad={mission} />
+      <div
+        className="character-cam"
+        style={{
+          left: `${left}px`,
+          bottom: `${bottom}px`,
+          position: "absolute",
+          zIndex: 1,
+        }}
+      >
+        <Charactercam setBottom={setBottom} setLeft={setLeft} />
       </div>
 
-        <img className="balloon study" src={png1} alt="숙제해" />
-        <img className="balloon hurry" src={png2} alt="잔소리2" />
-        <img className="balloon wash" src={png3} alt="잔소리3" />
+      <img className="balloon study" src={png1} alt="숙제해" />
+      <img className="balloon hurry" src={png2} alt="잔소리2" />
+      <img className="balloon wash" src={png3} alt="잔소리3" />
 
       <audio autoPlay>
         <source src={audio7} type="audio/mp3" />
