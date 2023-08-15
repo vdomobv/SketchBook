b1176eed172a74c51aa45d715162cc21c256930f
서정빈 <jeongb9716@gmail.com>
2023-08-15 14:14:30 +0900

    <feat>[#000-LJE] 캐릭터 이동에 따라 미션 수행 구현

--- front-end/src/pages/Play/Story1/P0/index.jsx
+++ front-end/src/pages/Play/Story1/P0/index.jsx
@@ -10,7 +10,7 @@ const P0 = () => {
 
   useEffect(() => {
     fetchNewImage(); // 컴포넌트가 마운트될 때 이미지 가져오기
-    const interval = setInterval(fetchNewImage, 50); // 200ms마다 이미지 업데이트
+    const interval = setInterval(fetchNewImage, 100); // 200ms마다 이미지 업데이트
     return () => clearInterval(interval); // 컴포넌트 언마운트 시 인터벌 클리어
   }, []);
 
