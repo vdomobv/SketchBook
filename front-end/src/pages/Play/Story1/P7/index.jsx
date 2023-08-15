08b9171a3ac91638afb8aba356c9e84c98060323
eazsilver <vdomobv@naver.com>
2023-08-15 16:20:52 +0900

    <fix>[#000-LJE] 미션 7 테스트 코드 삭제 기존 코드 복원

--- front-end/src/pages/Play/Story1/P7/index.jsx
+++ front-end/src/pages/Play/Story1/P7/index.jsx
@@ -4,6 +4,9 @@ import png1 from "../../../../play-png/7_잔소리1_숙제해.png";
 import png2 from "../../../../play-png/7_잔소리3_빨리빨리.png";
 import png3 from "../../../../play-png/7_잔소리5_씻어라.png";
 
+import boom1 from "../../../../play-background/success_clear.mp3";
+import boom2 from "../../../../play-background/success_yeah.mp3";
+
 import audio7 from "../../../../play-background/ske_7.mp3";
 import axios from "axios";
 // import Livecam from "../../../../components/Livecam";
@@ -13,66 +16,69 @@ import { useNavigate } from "react-router-dom";
 import checkOverlap from "../../../../utils/checkOverlap";
 
 let email;
-
 const Charactercam = (props) => {
   const [characterUrl, setcharacterUrl] = useState();
   const { setBottom, setLeft } = props;
 
-  const updatePosition = () => {
-    axios
-      .get("/api/devices/position")
-      .then((res) => {
-        email = res.data.email;
-        const x_diff = parseFloat(res.data.x_diff);
-        const y_diff = parseFloat(res.data.y_diff);
-
-        setBottom((prevBottom) => prevBottom + y_diff + y_diff);
-        setLeft((prevLeft) => prevLeft + x_diff + x_diff + x_diff);
-      })
-      .catch((err) => {
-        return console.log("에러입니다.", err);
-      });
-  };
-
   useEffect(() => {
     // 위치 정보 업데이트 함수
+    const updatePosition = () => {
+      axios
+        .get("/api/devices/position")
+        .then((res) => {
+          email = res.data.email;
+          const x_diff = parseFloat(res.data.x_diff);
+          const y_diff = parseFloat(res.data.y_diff);
+
+          setBottom((prevBottom) => prevBottom + y_diff);
+          setLeft((prevLeft) => prevLeft + x_diff);
+        })
+        .catch((err) => {
+          return console.log("에러입니다.", err);
+        });
+    };
 
     // 컴포넌트 마운트 시 처음 한 번 호출
     updatePosition();
 
     // 10초마다 API 호출
-    // const interval = setInterval(updatePosition, 100);
+    const interval = setInterval(updatePosition, 200);
 
     // 컴포넌트 언마운트 시 인터벌 정리
-    // return () => clearInterval(interval);
+    return () => clearInterval(interval);
   }, []);
 
   const fetchNewImage = () => {
     const timestamp = new Date().getTime();
-    // setcharacterUrl(`/assets/assemble.png?timestamp=${timestamp}`); //local
-    setcharacterUrl(`/user/${email}/assemble.png?timestamp=${timestamp}`); // 배포
+    setcharacterUrl(`/assets/assemble.png?timestamp=${timestamp}`); //local
+    // setcharacterUrl(`/user/${email}/assemble.png?timestamp=${timestamp}`); // 배포
   };
 
   useLayoutEffect(() => {
     fetchNewImage(); // 컴포넌트가 마운트될 때 이미지 가져오기
-    // const interval = setInterval(fetchNewImage, 100); // 200ms마다 이미지 업데이트
-    // return () => clearInterval(interval); // 컴포넌트 언마운트 시 인터벌 클리어
+    const interval = setInterval(fetchNewImage, 200); // 200ms마다 이미지 업데이트
+    return () => clearInterval(interval); // 컴포넌트 언마운트 시 인터벌 클리어
   });
 
-  return <img src={characterUrl} alt="" style={{ width: '768px', height: '576px' }} onLoad={() => { updatePosition(); fetchNewImage(); }} />;
+  return <img src={characterUrl} alt="" />;
 };
+
 
 function P7() {
   const navigate = useNavigate();
   const [stage, setStage] = useState(0);
   const [bottom, setBottom] = useState(0);
   const [left, setLeft] = useState(0);
+  const audioElement = new Audio(boom1);
+  const audio2Element = new Audio(boom2);
 
   useEffect(() => {
-    if (stage === 0 && checkOverlap("hurry")) {
+    if (stage === 0 && checkOverlap("study")) {
       setStage(1);
-    } else if (stage === 1 && checkOverlap("study")) {
+      audioElement.play();
+    } else if (stage === 1 && checkOverlap("hurry")) {
       setStage(2);
+      audioElement.play();
     } else if (stage === 2 && checkOverlap("wash")) {
       setStage(3);
     }
@@ -80,7 +86,10 @@ function P7() {
   }, [bottom, left, stage]);
 
   if (stage === 3) {
-    navigate("/play/story1/p8");
+    audio2Element.play();
+    audio2Element.onended = () => {
+      navigate("/play/story1/p8");
+    };
   }
 
   return (
@@ -132,10 +141,10 @@ function P7() {
       </div>
 
       {stage === 0 && (
-        <img id="hurry" className="balloon hurry" src={png2} alt="잔소리2" />
+        <img id="study" className="balloon study" src={png1} alt="숙제해" />
       )}
       {stage === 1 && (
-        <img id="study" className="balloon study" src={png1} alt="숙제해" />
+        <img id="hurry" className="balloon hurry" src={png2} alt="잔소리2" />
       )}
       {stage === 2 && (
         <img id="wash" className="balloon wash" src={png3} alt="잔소리3" />
