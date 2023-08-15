6a890558372254eb0321d403d8dbd169d1d9ad40
eazsilver <vdomobv@naver.com>
2023-08-15 14:46:16 +0900

    <fix>[#000-LJE] 미션 위치 및 순서 변경
    Former-commit-id: 6bc18870d46d8928a5d135403d1924f6ce587873

--- front-end/src/pages/Play/Story1/P7/styles.jsx
+++ front-end/src/pages/Play/Story1/P7/styles.jsx
@@ -11,20 +11,20 @@ const Wrapper = styled.div`
   // z-index: 1000;
 }
 
-// 1번째, 위 "숙제해"
-.study {
-  position: absolute;
-  top: 5%;
-  left: 50%;
-  transform: rotate(180deg);
-}
-
-// 2번째, 오른쪽 "빨리빨리"
+// 1번째, 오른쪽 "빨리빨리"
 .hurry {
   position: absolute;
   bottom: 5%;
   left: 75%;
   transform: rotate(260deg);
+}
+
+// 2번째, 위 "숙제해"
+.study {
+  position: absolute;
+  top: 20%;
+  left: 45%;
+  transform: rotate(180deg);
 }
 
 // 3번째, 왼쪽 "씻어라"
